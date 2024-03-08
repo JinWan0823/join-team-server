60f3ba5eb6dcce23b6d525bad9c52c4ef10b6871
jinWan0823 <dhksl823@gmail.com>
2024-03-08 21:12:50 +0900

    ✨ Feed 작성 post API 생성

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const { MongoClient, ObjectId } = require("mongodb");
 
@@ -59,6 +61,30 @@ app.get("/club/:id", async (req, res) => {
     res.json(result);
   } catch (error) {
     console.error("데이터 조회 오류:", error);
+    res.status(500).json({ error: "서버 오류 발생" });
+  }
+});
+
+app.post("/feed", async (req, res) => {
+  console.log(req.body);
+  const content = req.body.content;
+  const hashTag = req.body.hashTag;
+  const img = req.body.img;
+
+  try {
+    if (!content || hashTag.length === 0 || !img) {
+      res.status(400).json({ message: "Bad Request : 잘못된 요청입니다." });
+    } else {
+      await db.collection("feed").insertOne({
+        content: content,
+        hashTag: hashTag,
+        img: img,
+        date: new Date(),
+      });
+      res.status(201).json({ message: "데이터 등록 성공" });
+    }
+  } catch (error) {
+    console.error("데이터 등록 오류 : ", error);
     res.status(500).json({ error: "서버 오류 발생" });
   }
 });
